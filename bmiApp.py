9c1a6ab7dd0be67ab6fee7b83ae69cb58b5cb93c
406-34-atichon <54692@yupparaj.ac.th>
2026-07-31 10:59:28 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -4,7 +4,7 @@ st.markdown("# red[🏃แอปพลิเคชั่นคำนวณค่
 st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
 
 weight = st.number_input("กรอกน้ำหนักของคุณ ()กิโลกรัม:")
-height_cm = st.number_input("กรอกน้ำส่วนสูงของคุณ ()เซนติเมตร:")
+height_cm = st.number_input("กรอกส่วนสูงของคุณ ()เซนติเมตร:")
 
 if st.button("💪 คำนวณค่า BMI"):
   height_m = height_cm / 100
@@ -13,7 +13,7 @@ if st.button("💪 คำนวณค่า BMI"):
   st.write("---")
   st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
-if bmi < 18.5:
+if bmi < 18.5 :
   st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
 elif 18.5 <= bmi < 23.0 :
   st.success("✅ คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
