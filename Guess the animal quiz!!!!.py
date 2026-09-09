e89af241cdcea9f9b8f7b83e6ea7115276252fc7
406-34-atichon <54692@yupparaj.ac.th>
2026-09-09 22:46:43 +0700

    Update Guess the animal quiz!!!!.py

--- Guess the animal quiz!!!!.py
+++ Guess the animal quiz!!!!.py
@@ -218,7 +218,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
     
     # 5. แสดง Dialog ผลลัพธ์
-  if st.session_state.get("is_ended", False):
+if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans10)
 
 st.divider()
