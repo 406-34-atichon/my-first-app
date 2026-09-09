375b75f9a12699997501e6eb42bcc52a034cd432
406-34-atichon <54692@yupparaj.ac.th>
2026-09-09 22:16:09 +0700

    Update Guess the animal quiz!!!!.py

--- Guess the animal quiz!!!!.py
+++ Guess the animal quiz!!!!.py
@@ -63,7 +63,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
     # ตรวจข้อ 1
     if u_ans1 == "หมา" or u_ans1 == "สุนัข":    
         st.success("✅ ข้อ 1: ถูกต้อง")        
-    score += 1
+        score += 1
     else:    
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")        
 
