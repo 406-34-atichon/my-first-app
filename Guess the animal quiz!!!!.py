ce71a1a349ee7a39642322064e9c761a2b0bd433
406-34-atichon <54692@yupparaj.ac.th>
2026-09-11 10:24:33 +0700

    Update Guess the animal quiz!!!!.py

--- Guess the animal quiz!!!!.py
+++ Guess the animal quiz!!!!.py
@@ -61,14 +61,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
     u_ans9 = str(ans9 or "").strip().lower()
     u_ans10 = str(ans10 or "").strip().lower()    
     # ตรวจข้อ 1
-    if u_ans1 == "หมา" or u_ans1 == "สุนัข":
+    if u_ans1 in ["หมา", "สุนัข", "dog"]:  
         st.success("✅ ข้อ 1: ถูกต้อง")        
         score += 1
     else:    
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")        
 
     # ตรวจข้อ 2
-    if u_ans2 == "แมว" or u_ans2 == "cat":    
+    if u_ans2 in ["cat", "แมว", "เเมว"]:     
         st.success("✅ ข้อ 2: ถูกต้อง")        
         score += 1
     else:    
@@ -103,14 +103,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
          st.error(f"❌ ข้อ 6: ยังไม่ถูกต้อง (คุณตอบ '{u_ans6}')")        
                      
         # ตรวจข้อ 7
-    if u_ans7 in ["Cow", "วัว", "วัวนม"]:    
+    if u_ans7 in ["cow", "วัว", "วัวนม"]:    
         st.success("✅ ข้อ 7: ถูกต้อง")        
         score += 1
     else:    
         st.error(f"❌ ข้อ 7: ยังไม่ถูกต้อง (คุณตอบ '{u_ans7}')")        
     
         # ตรวจข้อ 8
-    if u_ans8 in ["กระต่าย", "Rabbit"]:    
+    if u_ans8 in ["กระต่าย", "rabbit"]:    
         st.success("✅ ข้อ 8: ถูกต้อง")        
         score += 1
     else:    
@@ -124,7 +124,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
         st.error(f"❌ ข้อ 9: ยังไม่ถูกต้อง (คุณตอบ '{u_ans9}')")        
     
         # ตรวจข้อ 10
-    if u_ans10 in ["salmon", "ปลาแซลม่อน", "แซลม่อน"]:
+    if u_ans10 in ["salmon", "ปลาแซลม่อน", "แซลม่อน", "แซลมอน", "ปลาแซลมอน", "เเซลมอน", "ปลาเเซลมอน", "เเซลม่อน", "ปลาเเซลม่อน"]:
         st.success("✅ ข้อ 10: ถูกต้อง")
         score += 1
     else:    
