9264f86c0d8481f430878f8f063feffeaddb4a17
406-34-atichon <54692@yupparaj.ac.th>
2026-09-11 10:26:13 +0700

    Update Guess the animal quiz!!!!.py

--- Guess the animal quiz!!!!.py
+++ Guess the animal quiz!!!!.py
@@ -1,7 +1,8 @@
 import time
 import streamlit as st
 
-st.title("⏱️ เกมทายศัพท์จับเวลา   (หมวดแอนิมอล)")
+st.title("⏱️ เกมทายศัพท์จับเวลา")
+st.title("(หมวดแอนิมอล)")
 
 # 1. กำหนดค่าเริ่มต้นใน session_state ถ้ายังไม่มี
 if "ans1_val" not in st.session_state:
